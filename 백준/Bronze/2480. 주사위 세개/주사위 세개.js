271d3a2e2c6d2af585d49274d5c8476f9f08aff8
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-05-13 21:26:09 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().trim().split(' ');
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+const c = parseInt(input[2]);
+
+
+if(a==b && b==c) {
+    console.log(10000+a*1000);
+} else if (a==b || b==c || a==c) {
+    a==b ? console.log(1000+a*100) : console.log(1000+c*100);
+} else {
+     console.log(Math.max(a,b,c)*100);
+}
