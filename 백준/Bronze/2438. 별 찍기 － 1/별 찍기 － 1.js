32d4f3db69aabb6e1496365855f0cbde25be8380
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-05-17 23:20:57 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 116 ms, Memory: 9484 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -2,9 +2,11 @@ const input = require('fs').readFileSync('dev/stdin').toString().trim();
 
 let star = "";
 
-for (let i = 0; i < parseInt(input); i++) {
+for (let i = 1; i <= input; i++) {
+  for (let j = 1; j <= i; j++) {
     star += "*";
-    console.log(star);
+  }
+  star += "\n";
 }
 
-
+console.log(star);
