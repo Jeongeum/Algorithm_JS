3d27c1dae52b720425162590070e174f38cd5ce7
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-05-18 17:18:06 +0900

    [Bronze III] Title: A+B - 4, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split("\n");
+
+let result = [];
+
+for (let i = 0; i < input.length; i++) {
+  const num = input[i].split(" ").map((x) => parseInt(x));
+  result.push(num[0] + num[1]);
+}
+console.log(result.join("\n"));
