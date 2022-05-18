0eac14005333e513e877e7874fe6abe5b70c96bd
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-05-18 22:51:40 +0900

    [Bronze I] Title: 더하기 사이클, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.js
@@ -0,0 +1,16 @@
+let input = require('fs').readFileSync('dev/stdin').toString().trim();
+
+let num = parseInt(input);
+let sum;
+let i = 0;
+
+while (true) {
+  i++;
+
+  sum = Math.floor(num / 10) + (num % 10);
+  num = (num % 10) * 10 + (sum % 10);
+  if (input == num) {
+    break;
+  }
+}
+console.log(i);
