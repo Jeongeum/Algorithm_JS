20691baca2cf8671021091318502b9a0ae5c3874
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-05-23 16:57:36 +0900

    [Bronze I] Title: 평균, Time: 120 ms, Memory: 9644 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -3,15 +3,9 @@ const input = require('fs').readFileSync('dev/stdin').toString().trim().split("\
 const N = parseInt(input[0]);
 const arr = input[1].split(" ").map((x) => parseInt(x));
 
-let max = 0;
+const max = Math.max(...arr);
+
 let sum = 0;
-
-for (let i = 0; i < N; i++) {
-  if (arr[i] > max) {
-    max = arr[i];
-  }
-}
-
 for (let j = 0; j < N; j++) {
   arr[j] = (arr[j] / max) * 100;
   sum += arr[j];
