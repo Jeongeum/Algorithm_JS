2ed4a0c5c740a5d294d7cef752a9ce19ffc1d828
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-05-24 19:36:19 +0900

    [Bronze II] Title: 숫자의 합, Time: 144 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -0,0 +1,10 @@
+const input = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
+
+const N = parseInt(input[0]);
+const arr = input[1].split("").map((x) => parseInt(x));
+let sum = 0;
+
+for (let i = 0; i < N; i++) {
+  sum += arr[i];
+}
+console.log(sum);
