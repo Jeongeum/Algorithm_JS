fa3c475e8bed6c06685cfd5868d857f9b1224ee2
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-08-18 16:04:58 +0900

    Update 팩토리얼.js

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.js
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -15,3 +15,24 @@ function factorial(n) {
   }
 }
 factorial(N);
+
+/* 수정 */
+const N = parseInt(input);
+
+function factorial(n) {
+  if (n === 0) return 1;
+  else return n * factorial(n - 1);
+}
+
+console.log(factorial(N));
+
+/* for문 */
+if (input === 0 && input === 1) return 1;
+
+let result = 1;
+
+for (let i = 1; i <= input; i++) {
+  result *= i;
+}
+
+console.log(result);
