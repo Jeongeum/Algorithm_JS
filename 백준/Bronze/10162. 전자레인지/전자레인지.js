27c1cda2e52d67145c4e37a6ea7d925c0b4686c3
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-08-22 23:23:22 +0900

    [Bronze III] Title: 전자레인지, Time: 168 ms, Memory: 9388 KB -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.js
+++ 백준/Bronze/10162. 전자레인지/전자레인지.js
@@ -1,7 +1,7 @@
 /* dev/stdin */
 let T = require("fs").readFileSync("dev/stdin").toString().trim();
 
-const button = [300, 60, 10];
+const button = [300, 60, 10]; 
 const count = [0, 0, 0];
 
 for (const i in button) {
@@ -11,4 +11,4 @@ for (const i in button) {
   }
 }
 
-console.log(T != 0 ? -1 : count.join(" "));
+console.log(T % 10 !== 0 ? -1 : count.join(" "));
