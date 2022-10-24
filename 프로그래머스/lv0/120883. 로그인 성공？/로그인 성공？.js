ddaa488299a2740cdd419d685593bd841ba6b41d
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-10-24 19:36:03 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,7 +1,7 @@
 function solution(id_pw, db) {
     
     let result = '';
-    db.forEach(data => {
+    db.map(data => {
         if(id_pw[0] === data[0] && id_pw[1] === data[1]) result = 'login'; 
         else if(id_pw[0] === data[0] && id_pw[1] !== data[1]) result = 'wrong pw';
         else if(id_pw[0] !== data[0] && id_pw[1] !== data[1]) {
