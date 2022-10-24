3d8d38a21407eb2c3b15c660ca59ac9a5f40e5ff
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-10-24 21:18:14 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,12 +1,37 @@
-function solution(id_pw, db) {
+// function solution(id_pw, db) {
     
-    let result = '';
+//     let result = '';
+//     db.map(data => {
+//         if(id_pw[0] === data[0] && id_pw[1] === data[1]) {
+//             console.log('로그인')
+//             result = 'login'; 
+//         }
+//         else if(id_pw[0] === data[0] && id_pw[1] !== data[1]) result = 'wrong pw';
+//         else if(id_pw[0] !== data[0] && id_pw[1] !== data[1]) {
+//             result = 'fail';
+//              }
+//     console.log(`data : ${data} 일 때, result = ${result}`)
+//     })
+//     return result;
+
+// }
+
+
+function solution (id_pw, db) {
+    let result ='';
     db.map(data => {
-        if(id_pw[0] === data[0] && id_pw[1] === data[1]) result = 'login'; 
-        else if(id_pw[0] === data[0] && id_pw[1] !== data[1]) result = 'wrong pw';
-        else if(id_pw[0] !== data[0] && id_pw[1] !== data[1]) {
-            result = 'fail';
-             }
+        let checkedId = id_pw[0] === data[0]
+        let checkedPw = id_pw[1] === data[1]
+       if(checkedId && checkedPw) {
+           return result = 'login'
+       } else if (checkedId && !checkedPw || !checkedId && checkedPw) {
+           return result = 'wrong pw'
+       } else {
+           return result = 'fail'
+       }
+        console.log(`data : ${data} 일 때, result = ${result}`)
     })
-    return result;
+    return result
 }
+// map 메서드를 쓰면서 db 내 중간 부분에서 아이디비번 일치한 결과로 최종 결과까지 가려면 맨 마지막 값은 비번이 같아야 한다.
+// 결국 
