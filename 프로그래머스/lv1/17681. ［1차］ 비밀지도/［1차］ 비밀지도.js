ab616e538f4e95102f7439ce87cd2aba400dff94
Jeongeum <77143425+Jeongeum@users.noreply.github.com>
2022-11-01 15:02:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,10 +1,8 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
-    for(let i = 0; i<n; i++) {
-        let row = (arr1[i] | arr2[i]).toString(2);
-        row = "0".repeat(n-row.length) + row;
-        row = row.replace(/[10]/g, num => +num ? "#" : " ")
-        answer.push(row);
+    let answer = []
+    const zip = (a, b) => a.map((v, i) => [v, b[i]]);
+    for ([i, j] of zip(arr1, arr2)){
+        answer.push((i | j).toString(2).padStart(n, '0').replace(/1/g, '#').replace(/0/g, ' '))
     }
-   return answer;
+    return answer
 }
